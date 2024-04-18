b5bab085cf1c407d44fbf738fd1fa75d1de89ea1
Muniox <muniox20@gmail.com>
2024-04-18 07:32:01 +0200

    An Important Note about Route Observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  paramsSuscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -16,8 +18,18 @@ export class UserComponent implements OnInit {
     //   id: this.route.snapshot.params['id'],
     //   name: this.route.snapshot.params['name'],
     // };
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSuscription = this.route.params.subscribe((params: Params) => {
       this.user = { id: params['id'], name: params['name'] };
     });
+  }
+
+  /**
+   * The ActivatedRoute and its observables are insulated from the Router itself.
+   * The Router destroys a routed component when it is no longer needed
+   * and the injected ActivatedRoute dies with it.
+   */
+
+  ngOnDestroy() {
+    this.paramsSuscription.unsubscribe(); //można to zrobić ale nie trzeba wytłumaczenie powyżej
   }
 }
